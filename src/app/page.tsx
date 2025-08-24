b829c8a4c3c831852b95190d21f1f160b1e15e50
zeyneptass <ztas7201@gmail.com>
2025-08-24 19:51:16 +0300

    İntro tasarlandı

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import logo from "../img/logo.svg";
 import menu from "../img/hamburger.svg";
+import profile from "../img/profile.png";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -19,7 +20,7 @@ export default function Home() {
 
   return (
     <div>
-      <header className="fixed w-full">
+      <header className="fixed w-full ">
         <nav className="bg-white border-gray-200 py-2.5">
           <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
             <a href="#" className="flex items-center">
@@ -63,6 +64,24 @@ export default function Home() {
           </div>
         </nav>
       </header>
+      <section className="bg-gray-100">
+        <div className="max-w-screen-xl mx-auto px-4 pt-30 pb-10 py-8 lg:pb-0 lg:pt-20 grid lg:grid-cols-12">
+          {/* left */}
+          <div className="lg:col-span-7 place-self-center">
+           <div className="text">
+            <h1 className="max-w-2xl mt-10 mb-4 text-4xl font-bold tracking-tight md:text-5xl xl:text-6xl">Hi, I'm John </h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus repudiandae ducimus iusto alias asperiores delectus dolore illo nostrum perspiciatis itaque. Cum reiciendis ipsam provident in distinctio magni ad perspiciatis eveniet?</p>
+           </div>
+           <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+            <a href="#" className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-purple-500 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">View on Github</a>
+           </div>
+          </div>
+          {/* right */}
+          <div className="hidden lg:flex lg:mt-0 lg:col-span-5">
+             <Image src={profile} alt="profile" className="w-full h-full object-cover" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
